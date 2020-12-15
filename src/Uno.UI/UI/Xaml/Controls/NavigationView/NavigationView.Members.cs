5888996d2441dc255075efb45ddcfbb412c707e9
Martin Zikmund <martin@zikmund.dev>
2020-12-15 22:47:21 +0100

    chore(NavigationView): Add excessive usings
    - Original NavigationView is excluded from WinUI revert
    - Added a lot of usings to avoid old navigation view from using Microsoft.UI.Xaml.Controls.NavigationViewXYZ

--- src/Uno.UI/UI/Xaml/Controls/NavigationView/NavigationView.Members.cs
+++ src/Uno.UI/UI/Xaml/Controls/NavigationView/NavigationView.Members.cs
@@ -9,6 +9,46 @@ using System.Collections.Generic;
 using Uno.Disposables;
 using Windows.ApplicationModel.Core;
 using Windows.UI.ViewManagement;
+#if HAS_UNO_WINUI
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Composition;
+using Microsoft.UI.Xaml.Automation;
+using Microsoft.UI.Xaml.Automation.Peers;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Hosting;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Media.Animation;
+using NavigationView = Windows.UI.Xaml.Controls.NavigationView;
+using NavigationViewAutomationPeer = Windows.UI.Xaml.Automation.Peers.NavigationViewAutomationPeer;
+using NavigationViewItemAutomationPeer = Windows.UI.Xaml.Automation.Peers.NavigationViewItemAutomationPeer;
+using NavigationViewBackButtonVisible = Windows.UI.Xaml.Controls.NavigationViewBackButtonVisible;
+using NavigationViewBackRequestedEventArgs = Windows.UI.Xaml.Controls.NavigationViewBackRequestedEventArgs;
+using NavigationViewDisplayMode = Windows.UI.Xaml.Controls.NavigationViewDisplayMode;
+using NavigationViewDisplayModeChangedEventArgs = Windows.UI.Xaml.Controls.NavigationViewDisplayModeChangedEventArgs;
+using NavigationViewItemHelper = Windows.UI.Xaml.Controls.NavigationViewItemHelper;
+using NavigationViewItem = Windows.UI.Xaml.Controls.NavigationViewItem;
+using NavigationViewItemBase = Windows.UI.Xaml.Controls.NavigationViewItemBase;
+using NavigationViewItemHeader = Windows.UI.Xaml.Controls.NavigationViewItemHeader;
+using NavigationViewItemInvokedEventArgs = Windows.UI.Xaml.Controls.NavigationViewItemInvokedEventArgs;
+using NavigationViewItemPresenter = Windows.UI.Xaml.Controls.Primitives.NavigationViewItemPresenter;
+using NavigationViewItemSeparator = Windows.UI.Xaml.Controls.NavigationViewItemSeparator;
+using NavigationViewList = Windows.UI.Xaml.Controls.NavigationViewList;
+using NavigationViewPaneClosingEventArgs = Windows.UI.Xaml.Controls.NavigationViewPaneClosingEventArgs;
+using NavigationViewPaneDisplayMode = Windows.UI.Xaml.Controls.NavigationViewPaneDisplayMode;
+using NavigationViewSelectionChangedEventArgs = Windows.UI.Xaml.Controls.NavigationViewSelectionChangedEventArgs;
+using NavigationViewTemplateSettings = Windows.UI.Xaml.Controls.NavigationViewTemplateSettings;
+using TopNavigationViewDataProvider = Windows.UI.Xaml.Controls.TopNavigationViewDataProvider;
+#else
+using Windows.UI.Composition;
+using Windows.UI.Xaml.Automation;
+using Windows.UI.Xaml.Automation.Peers;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Hosting;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Animation;
+#endif
 
 namespace Windows.UI.Xaml.Controls
 {
