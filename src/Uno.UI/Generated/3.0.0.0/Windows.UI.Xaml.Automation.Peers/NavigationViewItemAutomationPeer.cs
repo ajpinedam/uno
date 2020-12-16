b9792339a3b4d1ef576bf1cf8bc6a0c105a1f15e
Andres Pineda <pineda.andres@gmail.com>
2020-12-16 16:05:22 -0500

    fix: NavigationView

--- src/Uno.UI/Generated/3.0.0.0/Windows.UI.Xaml.Automation.Peers/NavigationViewItemAutomationPeer.cs
+++ src/Uno.UI/Generated/3.0.0.0/Windows.UI.Xaml.Automation.Peers/NavigationViewItemAutomationPeer.cs
@@ -1,5 +1,7 @@
 #pragma warning disable 108 // new keyword hiding
 #pragma warning disable 114 // new keyword hiding
+using Windows.UI.Xaml.Controls;
+
 namespace Windows.UI.Xaml.Automation.Peers
 {
 	#if false
@@ -7,13 +9,17 @@ namespace Windows.UI.Xaml.Automation.Peers
 	#endif
 	public  partial class NavigationViewItemAutomationPeer : global::Windows.UI.Xaml.Automation.Peers.ListViewItemAutomationPeer
 	{
-		#if false
+#if false
 		[global::Uno.NotImplemented("__ANDROID__", "__IOS__", "NET461", "__WASM__", "__SKIA__", "__NETSTD_REFERENCE__", "__MACOS__")]
 		public NavigationViewItemAutomationPeer( global::Windows.UI.Xaml.Controls.NavigationViewItem owner) : base(owner)
 		{
 			global::Windows.Foundation.Metadata.ApiInformation.TryRaiseNotImplemented("Windows.UI.Xaml.Automation.Peers.NavigationViewItemAutomationPeer", "NavigationViewItemAutomationPeer.NavigationViewItemAutomationPeer(NavigationViewItem owner)");
 		}
-		#endif
+#endif
+
+		public NavigationViewItemAutomationPeer(ListViewItem owner) : base(owner)
+		{
+		}
 		// Forced skipping of method Windows.UI.Xaml.Automation.Peers.NavigationViewItemAutomationPeer.NavigationViewItemAutomationPeer(Windows.UI.Xaml.Controls.NavigationViewItem)
 	}
 }
